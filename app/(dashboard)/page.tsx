57c2a867c47eacbc5f9e226c38f30fa1985ee39e
unknown <hakimpogba6@gmail.com>
2026-01-10 22:38:33 +0800

    finished charts on Overview Page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,37 +1,39 @@
-"use client";
+// "use client";
 
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
-import { Button } from "@/components/ui/button";
-import { SignOutButton, UserButton } from "@clerk/nextjs";
-import {db} from "@/src/db";
-import { accounts } from "@/db/schema";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-// export default async function Home() {
-export default function Home() {
-  const { onOpen } = useNewAccount();
 
-  // const result = await db.select().from(accounts);
-  // const result1 = await db.execute("select 1 as connected");
-  // const rows = result1.rows;
-  return (
-     <nav className="flex justify-between items-center p-4 bg-gray-100 shadow">
-      <h1 className="text-2xl font-semibold">My App</h1>
-      <div>
-        <Button onClick={onOpen}>
-          Add an account
-        </Button>
-      </div>
-      {/* <h1>Drizzle + Neon Test</h1>
-    <pre>{JSON.stringify(result, null, 2)}</pre> */}
+// import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
-    <SignOutButton redirectUrl="/">
-      <button className="px-4 py-2 bg-red-500 text-white rounded-md">
-        Sign Out
-      </button>
-    </SignOutButton></nav>
-  )
-}
+// import { Button } from "@/components/ui/button";
+// import { SignOutButton, UserButton } from "@clerk/nextjs";
+// import {db} from "@/src/db";
+// import { accounts } from "@/db/schema";
+// import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+// // export default async function Home() {
+// export default function Home() {
+//   const { onOpen } = useNewAccount();
+
+//   // const result = await db.select().from(accounts);
+//   // const result1 = await db.execute("select 1 as connected");
+//   // const rows = result1.rows;
+//   return (
+//      <nav className="flex justify-between items-center p-4 bg-gray-100 shadow">
+//       <h1 className="text-2xl font-semibold">My App</h1>
+//       <div>
+//         <Button onClick={onOpen}>
+//           Add an account
+//         </Button>
+//       </div>
+//       {/* <h1>Drizzle + Neon Test</h1>
+//     <pre>{JSON.stringify(result, null, 2)}</pre> */}
+
+//     <SignOutButton redirectUrl="/">
+//       <button className="px-4 py-2 bg-red-500 text-white rounded-md">
+//         Sign Out
+//       </button>
+//     </SignOutButton></nav>
+//   )
+// }
 
 
 // export default function Home() {
@@ -57,5 +59,15 @@ export default function Home() {
 // };
 
 
+import { DataCharts } from "@/components/data-charts";
+import { DataGrid } from "@/components/data-grid";
 
+export default function DashboardPage() {
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
+      <DataCharts />
+    </div>
+  );
+};
 
